e6812f2bbe1ce4a5a29c882f75111233bd3e8e86
Paula De Amicis <paula_dea@live.com>
2023-06-13 10:42:29 +1000

    Update script.js with more coding and corrected structure

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 ﻿
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('result');
-const submitButton = document.getElementById('submit');
-
-//declare variables to store references
+// Functions
 
 function buildQuiz() {
   // variable to store the HTML output
@@ -11,33 +7,46 @@ function buildQuiz() {
 
   // for each question...
 
-  myQuestions.forEach(
-    (currentQuestion, questionNumber) => {
-    
-    // variable to store the list of possible answers
-          const answers = [];
+    myQuestions.forEach(
+        (currentQuestion, questionNumber) => {
 
-          // and for each available answer...
-          for (letter in currentQuestion.answers) {
+            // variable to store the list of possible answers
+            const answers = [];
 
-              // ... add an HTML text box
-              answers.push(
-                  '<label>
-                  < input type = "text" name ="question${questionNumber}">'
-              )
-          }
-    }
-    )
+            // and for each available answer...
+            for (letter in currentQuestion.answers) {
+
+                // ... add an HTML radio button
+                answers.push(
+                    `<label>
+                  < input type = "radio" name = "question${questionNumber}" value = "${letter}" >
+              ${letter} :
+                  ${currentQuestion.answers[letter]}
+                  </label>`
+                );
+            }
+
+            // add this question and its answers to the output by using template literals
+            //  using a template literal and some embedded expressions to first create the question div and then create the answer div
+            // the 'join' expression takes the list of answers and puts them together in one string that can be output into the 'answers' div
+            output.push(
+                `<div class="#"> ${currentQuestion.question} </div>
+              <div class="answers"> ${answers.join('')} </div>`
+            );
+        }
+    );
+
+    //finally, combine our output list into one string of HTML and put it on the page
+    quizContainer.innerHTML = output.join('');
 
 }
 
 function showResults() { }
 
-//run buildQuiz function immediately 
-buildQuiz();
-
-//on submit, show the result
-submitButton.addEventListener('click', showResults);
+// declare variables to store references
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('result');
+const submitButton = document.getElementById('submit');
 
 //display the quiz questions
 //using *object literals* to represent the individual questions and an array to hold all of the questions that make up the quiz, will be easier to iterate over//
@@ -45,31 +54,178 @@ submitButton.addEventListener('click', showResults);
 const myQuestions = [
 
     {
-        question: "It can be lost, given, stolen or broken may times in your life, yet if you are without it for a minute you will die. What is it?",
-
-        correctAnswer: "HEART"
+        question: "What is the name of the company that published the Mario Kart video game?",
+        answers: {
+            a: "Nintendo",
+            b: "Electronic Arts (EA)",
+            c: "SEGA",
+            d: "Xbox"
+        },
+        correctAnswer: "a"
     },
 
     {
-        question: "Add one word to make the following correct: 99 + 9 = 9 ",
-
-        correctAnswer: "DOZEN"
+        question: "How many elements are there on a periodic table?",
+        answers: {
+            a: "112",
+            b: "118",
+            c: "120",
+            d: "143"
+        },
+        correctAnswer: "b"
 
     },
 
     {
-        question: "Which 4-letter word links all of these? STEP  LOOSE  SORE  HOLD  LIGHTS",
+        question: "In the U.S version of The Office, Michael Scott burns his foot on: ",
+        answers: {
+            a: "hot water",
+            b: "pavement/cement",
+            c: "rocks on fire",
+            d: "a George Foreman Grill"
+        },
+        correctAnswer: "d"
 
-        correctAnswer: "FOOT"
+    },
 
-    }
+    {
+        question: "Which actor performs music under the stage name Childish Gambino?",
+        answers: {
+            a: "Donald Glover",
+            b: "Will Smith",
+            c: "Frank Ocean",
+            d: "Tyler, The Creator"
+        },
+        correctAnswer: "a"
+    },
 
+    {
+        question: "As an adult, how many teeth should you have in your mouth?",
+        answers: {
+            a: "35",
+            b: "32",
+            c: "30",
+            d: "42"
+        },
+        correctAnswer: "b"
+    },
 
+    {
+        question: "Which country held the 2016 Summer Olympics?",
+        answers: {
+            a: "China",
+            b: "Ireland",
+            c: "Brazil",
+            d: "italy"
+        },
+        correctAnswer: "c"
+    },
 
+    {
+        question: "Who painted the Sistine Chapel ceiling?",
+        answers: {
+            a: "Picasso",
+            b: "Da Vinci",
+            c: "Michaelangelo",
+            d: "Van Gogh"
+        },
+        correctAnswer: "c"
+    },
 
+    {
+        question: "In 2016, a musician won the Nobel Peace Prize for Literature. Who was it?",
+        answers: {
+            a: "Lenny Kravitz",
+            b: "Eric Clapton",
+            c: "Bob Dylan",
+            d: "Elton John"
+        },
+        correctAnswer: "c"
+    },
 
+    {
+        question: "What is Sodium Chloride?",
+        answers: {
+            a: "Salt",
+            b: "Sugar",
+            c: "Chlorine",
+            d: "Bleach"
+        },
+        correctAnswer: "a"
+    },
+
+    {
+        question: "How many time zones are there in the world?",
+        answers: {
+            a: "7",
+            b: "24",
+            c: "23",
+            d: "9"
+        },
+        correctAnswer: "b"
+    },
+
+    {
+        question: "What is the deadliest snake?",
+        answers: {
+            a: "Python",
+            b: "Cobra",
+            c: "Eastern Brown Snake",
+            d: "Black Mamba"
+        },
+        correctAnswer: "d"
+    },
+
+    {
+        question: "In Greek mythology, who is the Queen of the Underworld?",
+        answers: {
+            a: "Pandora",
+            b: "Medusa",
+            c: "Helen",
+            d: "Persephone"
+
+        },
+        correctAnswer: "b"
+    },
+
+    {
+        question: "Which biblical narrative is connected to Palm Sunday?",
+        answers: {
+            a: "Jesus' entry into Jerusalem",
+            b: "Jesus' resurrection",
+            c: "Jesus feeding the thousands",
+            d: "Nothing, it just means to go to church on Sunday - the day of rest"
+        },
+        correctAnswer: "a"
+    },
+
+    {
+        question: "Which planet is the hottest?",
+        answers: {
+            a: "Venus",
+            b: "Saturn",
+            c: "Mercury",
+            d: "Mars"
+        },
+        correctAnswer: "a"
+    },
+
+    {
+        question: "Which poet wrote the poem 'The Raven'?",
+        answers: {
+            a: "Robert Frost",
+            b: "Edgar Allan Poe",
+            c: "Walt Whitman",
+            d: "Sylvia Plath"
+        },
+        correctAnswer: "b"
+    },
 ];
 
+// Kick things off 
+buildQuiz();
 
+// Event listener: on submit, show the result
+submitButton.addEventListener('click', showResults);
 
 
