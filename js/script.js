6e0fa9881b069ba97d502afde67c0fa096d96ddd
Paula De Amicis <paula_dea@live.com>
2023-06-16 15:28:14 +1000

    building out showResults function

--- js/script.js
+++ js/script.js
@@ -32,17 +32,51 @@ function buildQuiz() {
 
             output.push(
                 `<div class="question"> ${currentQuestion.question} </div>
-              <div class="answers"> ${answers.join('')} </div>`
+              <div class="answers"> ${answers.join("")} </div>`
             );
         }
     );
 
     //finally, combine our output list into one string of HTML and put it on the page
     quizContainer.innerHTML = output.join('');
-
 }
 
-function showResults() { }
+function showResults() {
+
+// gather answer containers from our quiz
+    const answerContainers = quizContainer.querySelectorAll('.answers');
+
+    // keep track of user's answers
+    let numCorrect = 0;
+
+    // for each question...
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+
+        // find selected answer
+        const answerContainer = answerContainers[questionNumber];
+        const selector = `input[name=question${questionNumber}]:checked`;
+        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+        // if answer is correct
+        if (userAnswer === currentQuestion.correctAnswer) {
+            // add to the number of correct answers
+            numCorrect++;
+
+            // colour the answers green
+            answerContainers[questionNumber].style.color = 'lightgreen';
+        }
+
+        // if answer is wrong or blank
+        else {
+            // colour the answers red
+            answerContainers[questionNumber].style.color = 'red';
+        }
+    });
+
+    // show number of correct answers out of total
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+
+}
 
 // declare variables to store references
 const quizContainer = document.getElementById('quiz');
