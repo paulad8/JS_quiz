498261c3f73c2339fa2757ba817aad7720258ff3
Paula De Amicis <paula_dea@live.com>
2023-08-03 12:59:50 +1000

    Create questions.js
    Split-out questions from main.js

---
+++ js/questions.js
@@ -0,0 +1,266 @@
+﻿// All quiz questions listed below
+
+const easyQuestions = [
+
+    {
+        question: "Where are the Whitsunday Islands located?",
+        answers: {
+            a: "Malaysia",
+            b: "Vietnam",
+            c: "Philippines",
+            d: "Australia"
+        },
+        correctAnswer: "d"
+    },
+
+    {
+        question: "Which of these countries ISN’T landlocked?",
+        answers: {
+            a: "Nepal",
+            b: "Turkey",
+            c: "Armenia",
+            d: "Austria"
+        },
+        correctAnswer: "b"
+
+    },
+
+    {
+        question: "The Spanish Steps are found in which city?",
+        answers: {
+            a: "Rome",
+            b: "Barcelona",
+            c: "Madrid",
+            d: "Lisbon"
+        },
+        correctAnswer: "a"
+
+    },
+
+    {
+        question: "Which two countries in South America are the Iguazu Falls part of?",
+        answers: {
+            a: "Ethiopia and Kenya",
+            b: "Panama and Chile",
+            c: "Brazil and Argentina",
+            d: "Colombia and Brazil"
+        },
+        correctAnswer: "c"
+    },
+
+    {
+        question: "Which of these is NOT a South African national park? ",
+        answers: {
+            a: "Addo Elephant Park",
+            b: "Kruger National Park",
+            c: "Table Mountain National Park",
+            d: "Goreme National Park"
+        },
+        correctAnswer: "d"
+    },
+
+    {
+        question: "Where in the Middle East is the ancient city of Petra?",
+        answers: {
+            a: "Qatar",
+            b: "Jordan ",
+            c: "Egypt",
+            d: "Morocco"
+        },
+        correctAnswer: "b"
+    },
+
+    {
+        question: "The Aruba Caribbean island is a territory of which European country?",
+        answers: {
+            a: "France",
+            b: "Netherlands",
+            c: "Finland",
+            d: "Croatia"
+        },
+        correctAnswer: "b"
+    },
+
+    {
+        question: "What is the capital of Canada?",
+        answers: {
+            a: "Toronto",
+            b: "Vancouver",
+            c: "Ottawa",
+            d: "Halifax"
+        },
+        correctAnswer: "c"
+    },
+
+    {
+        question: "Hanoi is the capital of which country?",
+        answers: {
+            a: "Cambodia",
+            b: "Laos",
+            c: "Thailand",
+            d: "Vietnam"
+        },
+        correctAnswer: "d"
+    },
+
+    {
+        question: "Which of these buildings is the tallest? ",
+        answers: {
+            a: "The Shard, London",
+            b: "Tokyo Skytree, Tokyo",
+            c: "Burj Kalifa, Dubai",
+            d: "Ostankino Tower, Moscow"
+        },
+        correctAnswer: "c"
+    },
+
+    {
+        question: "The Baltic States refer to which countries?",
+        answers: {
+            a: "Iceland, Norway, UK",
+            b: "Poland, Czech Republic, Austria",
+            c: "Estonia, Latvia, Lithuania",
+            d: "Croatia, Slovenia, Italy"
+        },
+        correctAnswer: "c"
+    },
+
+    {
+        question: "Which city is home to the Brandenburg Gate?",
+        answers: {
+            a: "Vienna",
+            b: "Bruge",
+            c: "Zurich",
+            d: "Berlin"
+
+        },
+        correctAnswer: "d"
+    },
+
+    {
+        question: "Where was the first example of paper money used?",
+        answers: {
+            a: "China",
+            b: "Greece",
+            c: "Turkey",
+            d: "Britannia"
+        },
+        correctAnswer: "a"
+    },
+
+    {
+        question: "Which language has the longest alphabet?",
+        answers: {
+            a: "Greek",
+            b: "Arabic",
+            c: "Roman",
+            d: "Russian"
+        },
+        correctAnswer: "d"
+    },
+
+    {
+        question: "Which of the following was considered one of the Seven Ancient Wonders?",
+        answers: {
+            a: "Colosseum",
+            b: "Great Wall of China",
+            c: "Colossus of Rhodes",
+            d: "Ancient city of Ephesus"
+        },
+        correctAnswer: "c"
+    },
+];
+
+const hardQuestions = [
+
+    {
+        question: "What 'Z' river, the fourth-longest in Africa, originates in Zambia and flows east through Zimbabwe and Mozambique before emptying into the Indian Ocean?",
+
+        correctAnswer: "Zambezi"
+    },
+
+    {
+        question: "Busan, formerly called Pusan, is the second most populous city in which Asian country?",
+
+        correctAnswer: "South Korea"
+    },
+
+    {
+        question: "Khartoum, a metropolitan city on the confluence of the White Nile, is the capital city of what African nation on continent’s east coast?",
+
+        correctAnswer: "Sudan"
+    },
+
+    {
+        question: "Which of the current seven continents on Earth is believed to be the oldest in terms of how long humans have lived there (somewhere in the ballpark of 5 million years)?",
+
+        correctAnswer: "Africa"
+    },
+
+    {
+        question: "In what nation can you find the Coober Pedy, an opal mining town where summers are so hot, most of the town is built underground?",
+
+        correctAnswer: "Australia"
+    },
+
+    {
+        question: "Designed by starchitect Renzo Piano, The Shard is a 72-story skyscraper known for its needle-like shape, in what world capital city?",
+
+        correctAnswer: "London"
+    },
+
+    {
+        question: "Rome's Cloaca Maxima was an early example of what infrastructure project that makes cities a whole lot more liveable?",
+
+        correctAnswer: "Sewer system"
+    },
+
+    {
+        question: "Although the vast majority of the Amazon River is located in Brazil, the headwaters of the waterway are found in what other South American country?",
+
+        correctAnswer: "Peru"
+
+    },
+
+    {
+        question: "New Zealand is, unsurprisingly, named after another physical location. Zeeland is the least populous province in what European country?",
+
+        correctAnswer: "Netherlands"
+    },
+
+    {
+        question: "By some accounts, Mt. Everest isn't actually the tallest mountain in the world. What dormant volcano in the Pacific is actually 10,060 mt. (33,000 ft.) tall - 1,219 mt. (4,000 ft.) above Everest, although about half of that is underwater?",
+
+        correctAnswer: "Mauna Kea"
+    },
+
+    {
+        question: "The official languages of the United Nations are comprised of Arabic, Chinese, English, French, Spanish and what sixth language?",
+
+        correctAnswer: "Russian"
+    },
+
+    {
+        question: "The third-largest city in India is also the capital of the state of Karnataka. What is this city in south India? As a hint, the city shares the same first four letters with the 8th most populous country in the world.",
+
+        correctAnswer: "Bangalore"
+    },
+
+    {
+        question: "What river, located entirely within China, is the longest river in Asia and the longest river in the world to flow entirely within one country?",
+
+        correctAnswer: "Yangtze"
+    },
+
+    {
+        question: "Machu Picchu was never plundered by the Spanish because the conquistadors never found this ancient site from the height of the Inca Empire. The lack of discovery was especially unlikely because the site is only 80 km (50 miles) from what Inca capital city?",
+
+        correctAnswer: "Cusco"
+    },
+
+    {
+        question: "Located on the Yucatan Peninsula, what Mexican state sounds like it might contain Australian mammals but is actually named for a lawyer who fought for Mexican independence?",
+
+        correctAnswer: "Quintana Roo"
+    },
+];
