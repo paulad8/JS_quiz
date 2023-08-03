ee18161c3b22df1c2e1bb5056a3a2c7f0ea8e178
Paula De Amicis <paula_dea@live.com>
2023-08-03 13:00:21 +1000

    Create main.js
    some variables and event listeners created

---
+++ js/main.js
@@ -0,0 +1,32 @@
+﻿// Variables
+
+const difficultyEasyBtn = document.getElementById("difficulty-easy");
+const difficultyHardBtn = document.getElementById("difficulty-hard");
+
+const startGameArea = document.getElementById("start-game-area");
+const difficultyGameArea = document.getElementById("difficulty-game-area");
+
+
+    let gameState = "start-game-area";
+
+// Event Listeners
+difficultyEasyBtn.addEventListener("click", runQuiz);
+
+document.getElementById("play").addEventListener("click", toDifficultyGameArea);
+
+//
+
+/** When the play clicks 'Let's Go!', hides the start-game-area and shows the difficulty-game-area **/
+
+function toDifficultyGameArea() {
+    startGameArea.classList.add("hide");
+    difficultyGameArea.classList.remove("hide");
+    gameState = "difficulty-game-area";
+
+}
+
+/** Depending on what difficulty the player selects, this function ... 
+*/
+
+
+
