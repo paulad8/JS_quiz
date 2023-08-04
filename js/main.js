e51704082f3bb889521573626f5da0883df76819
Paula De Amicis <paula_dea@live.com>
2023-08-04 12:56:44 +1000

    Added more variables and created new runQuiz function

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
 ﻿// Variables
 
-const difficultyEasyBtn = document.getElementById("difficulty-easy");
-const difficultyHardBtn = document.getElementById("difficulty-hard");
-
 const startGameArea = document.getElementById("start-game-area");
 const difficultyGameArea = document.getElementById("difficulty-game-area");
+const questionGameArea = document.getElementById("question-game-area");
+const difficultyEasyBtn = document.getElementById("difficulty-easy");
+const difficultyHardBtn = document.getElementById("difficulty-hard");
+const nextQuestionBtn = document.getElementById("next-question");
+const answer1 = document.getElementById("answer1-btn");
+const answer2 = document.getElementById("answer2-btn");
+const answer3 = document.getElementById("answer3-btn");
+const answer4 = document.getElementById("answer4-btn");
+const currentQuestionNumber = document.getElementById("show-current-question");
+const questionText = document.getElementById("question-text");
 
 
-    let gameState = "start-game-area";
+
+let gameState = "start-game-area";
+let currentQuestion = 0;
+let shuffledQuestions = 0;
+let currentQuestionSet = {};
 
 // Event Listeners
 difficultyEasyBtn.addEventListener("click", runQuiz);
@@ -25,8 +36,26 @@ function toDifficultyGameArea() {
 
 }
 
-/** Depending on what difficulty the player selects, this function ... 
+/** Depending on what difficulty the player selects, this function will run the quiz
 */
+
+function runQuiz()
+    let difficulty = event.target.value;
+
+if (difficulty === "easy") {
+    shuffledQuestions = easyQuestions.sort(() => Math.random() - 0.5);
+    currentQuestionSet = shuffledQuestions;
+    playerDifficulty = "easy";
+    difficultyEasyBtn.innerText = "Loading...";
+} else if (difficulty === "hard") {
+    shuffledQuestions = hardQuestions.sort(() => Math.random() - 0.5);
+    currentQuestionSet = shuffledQuestions;
+    playerDifficulty = "hard";
+    difficultyHardBtn.innerText = "Loading...";
+}
+
+
+
 
 
 
