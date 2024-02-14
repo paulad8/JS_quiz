92aeab1ecad4e47d58f39ceefc95f8da20491bc5
Paula De Amicis <paula_dea@live.com>
2024-02-14 17:12:18 +1000

    changed structure of question and answer.

--- js/questions.js
+++ js/questions.js
@@ -4,170 +4,140 @@ const easyQuestions = [
 
     {
         question: "Where are the Whitsunday Islands located?",
-        answers: {
+        
             a: "Malaysia",
             b: "Vietnam",
             c: "Philippines",
-            d: "Australia"
-        },
-        correctAnswer: "d"
+            d: "Australia",
+        answer: "d",
     },
 
     {
         question: "Which of these countries ISN’T landlocked?",
-        answers: {
             a: "Nepal",
             b: "Turkey",
             c: "Armenia",
-            d: "Austria"
-        },
-        correctAnswer: "b"
+            d: "Austria",
+        answer: "b",
 
     },
 
     {
         question: "The Spanish Steps are found in which city?",
-        answers: {
             a: "Rome",
             b: "Barcelona",
             c: "Madrid",
-            d: "Lisbon"
-        },
-        correctAnswer: "a"
+            d: "Lisbon",
+        answer: "a",
 
     },
 
     {
         question: "Which two countries in South America are the Iguazu Falls part of?",
-        answers: {
             a: "Ethiopia and Kenya",
             b: "Panama and Chile",
             c: "Brazil and Argentina",
-            d: "Colombia and Brazil"
-        },
-        correctAnswer: "c"
+            d: "Colombia and Brazil",
+        answer: "c",
     },
 
     {
         question: "Which of these is NOT a South African national park? ",
-        answers: {
             a: "Addo Elephant Park",
             b: "Kruger National Park",
             c: "Table Mountain National Park",
-            d: "Goreme National Park"
-        },
-        correctAnswer: "d"
+            d: "Goreme National Park",
+        answer: "d",
     },
 
     {
         question: "Where in the Middle East is the ancient city of Petra?",
-        answers: {
             a: "Qatar",
             b: "Jordan ",
             c: "Egypt",
-            d: "Morocco"
-        },
-        correctAnswer: "b"
+            d: "Morocco",
+        answer: "b",
     },
 
     {
         question: "The Aruba Caribbean island is a territory of which European country?",
-        answers: {
             a: "France",
             b: "Netherlands",
             c: "Finland",
-            d: "Croatia"
-        },
-        correctAnswer: "b"
+            d: "Croatia",
+        answer: "b",
     },
 
     {
         question: "What is the capital of Canada?",
-        answers: {
             a: "Toronto",
             b: "Vancouver",
             c: "Ottawa",
-            d: "Halifax"
-        },
-        correctAnswer: "c"
+            d: "Halifax",
+        answer: "c",
     },
 
     {
         question: "Hanoi is the capital of which country?",
-        answers: {
             a: "Cambodia",
             b: "Laos",
             c: "Thailand",
-            d: "Vietnam"
-        },
-        correctAnswer: "d"
+            d: "Vietnam",
+        answer: "d",
     },
 
     {
         question: "Which of these buildings is the tallest? ",
-        answers: {
             a: "The Shard, London",
             b: "Tokyo Skytree, Tokyo",
             c: "Burj Kalifa, Dubai",
-            d: "Ostankino Tower, Moscow"
-        },
-        correctAnswer: "c"
+            d: "Ostankino Tower, Moscow",
+        answer: "c",
     },
 
     {
         question: "The Baltic States refer to which countries?",
-        answers: {
             a: "Iceland, Norway, UK",
             b: "Poland, Czech Republic, Austria",
             c: "Estonia, Latvia, Lithuania",
-            d: "Croatia, Slovenia, Italy"
-        },
-        correctAnswer: "c"
+            d: "Croatia, Slovenia, Italy",
+        answer: "c",
     },
 
     {
         question: "Which city is home to the Brandenburg Gate?",
-        answers: {
             a: "Vienna",
             b: "Bruge",
             c: "Zurich",
-            d: "Berlin"
-
-        },
-        correctAnswer: "d"
+            d: "Berlin",
+        answer: "d",
     },
 
     {
         question: "Where was the first example of paper money used?",
-        answers: {
             a: "China",
             b: "Greece",
             c: "Turkey",
-            d: "Britannia"
-        },
-        correctAnswer: "a"
+            d: "Britannia",
+        answer: "a",
     },
 
     {
         question: "Which language has the longest alphabet?",
-        answers: {
             a: "Greek",
             b: "Arabic",
             c: "Roman",
-            d: "Russian"
-        },
-        correctAnswer: "d"
+            d: "Russian",
+        answer: "d",
     },
 
     {
         question: "Which of the following was considered one of the Seven Ancient Wonders?",
-        answers: {
             a: "Colosseum",
             b: "Great Wall of China",
             c: "Colossus of Rhodes",
-            d: "Ancient city of Ephesus"
-        },
-        correctAnswer: "c"
+            d: "Ancient city of Ephesus",
+        answer: "c",
     },
 ];
 
@@ -175,186 +145,136 @@ const hardQuestions = [
 
     {
         question: "What 'Z' river, the fourth-longest in Africa, originates in Zambia and flows east through Zimbabwe and Mozambique before emptying into the Indian Ocean?",
-        answers: {
             a: "Zambezi",
             b: "Zigzag",
             c: "Zezere",
-            d: "Zeland"
-        },
-
-        correctAnswer: "a"
+            d: "Zeland",
+        answer: "a",
     },
 
     {
         question: "Busan, formerly called Pusan, is the second most populous city in which Asian country?",
-        answers: {
             a: "Shanghai",
             b: "Shenzhen",
             c: "Seoul",
-            d: "South Korea"
-
-        },
-
-        correctAnswer: "d"
+            d: "South Korea",
+        answer: "d",
     },
 
     {
         question: "Khartoum, a metropolitan city on the confluence of the White Nile, is the capital city of what African nation?",
-        answers: {
             a: "Addis Ababa",
             b: "Rabat",
             c: "Sudan",
-            d: "Dakar"
-
-        },
-
-        correctAnswer: "c"
+            d: "Dakar",
+        answer: "c",
     },
 
     {
         question: "Which of the current seven continents on Earth is believed to be the oldest in terms of how long humans have lived there (somewhere in the ballpark of 5 million years)?",
-        answers: {
             a: "Americas",
             b: "Africa",
             c: "Australasia",
-            d: "Western Asia"
-
-        },
-
-        correctAnswer: "b"
+            d: "Western Asia",
+        answer: "b",
     },
 
     {
         question: "In what nation can you find the Coober Pedy, an opal mining town where summers are so hot, most of the town is built underground?",
-        answers: {
             a: "U.S.A",
             b: "Kiribati",
             c: "Australia",
-            d: "Palau"
-
-        },
-
-        correctAnswer: "c"
+            d: "Palau",
+        answer: "c",
     },
 
     {
         question: "Designed by starchitect Renzo Piano, The Shard is a 72-story skyscraper known for its needle-like shape, in what world capital city?",
-        answers: {
             a: "London",
             b: "Chicago",
             c: "San Francisco",
-            d: "Melbourne"
-        },
-
-        correctAnswer: "a"
+            d: "Melbourne",
+        answer: "a",
     },
 
     {
         question: "Rome's Cloaca Maxima was an early example of what infrastructure project that makes cities a whole lot more liveable?",
-        answers: {
             a: "Roads",
             b: "Sewer system",
             c: "Aqueducts",
-            d: "Public baths"
-        },
-        
-        correctAnswer: "b"
+            d: "Public baths",
+        answer: "b",
     },
 
     {
         question: "Although the vast majority of the Amazon River is located in Brazil, the headwaters of the waterway are found in what other South American country?",
-        answers: {
             a: "Bolivia",
             b: "Paraguay",
             c: "Peru",
-            d: "Colombia"
-
-        },
-
-        correctAnswer: "c"
-
+            d: "Colombia",
+        answer: "c",
     },
 
     {
         question: "New Zealand is, unsurprisingly, named after another physical location. Zeeland is the least populous province in what European country?",
-        answers: {
             a: "Netherlands",
             b: "Denmark",
             c: "Sweden",
-            d: "Belgium"
-        }
-
-        correctAnswer: "a"
+            d: "Belgium",
+        answer: "a",
     },
 
     {
         question: "By some accounts, Mt. Everest isn't actually the tallest mountain in the world. What dormant volcano is actually 10,060 mt. (33,000 ft.) tall - 1,219 mt. (4,000 ft.) above Everest, although about half of that is underwater?",
-        answers: {
             a: "Mauna Kea",
             b: "Mount Overlord",
             c: "Mauna Loa",
-            d: "Mount Sidley"
-        },
-
-        correctAnswer: "a"
+            d: "Mount Sidley",
+        answer: "a",
     },
 
     {
         question: "The official languages of the United Nations are comprised of Arabic, Chinese, English, French, Spanish and what sixth language?",
-        answers: {
             a: "Hindi",
             b: "Mandarin",
             c: "Russian",
-            d: "Bengali"
-        },
-
-        correctAnswer: "c"
+            d: "Bengali",
+        answer: "c",
     },
 
     {
         question: "The third-largest city in India is also the capital of the state of Karnataka. What is this city in south India?",
-        answers: {
             a: "Mumbai",
             b: "Bangalore",
             c: "Kolkata",
-            d: "Hyderabad"
-        },
-        correctAnswer: "b"
+            d: "Hyderabad",
+        answer: "b",
     },
 
     {
         question: "What river is the longest river in Asia and the longest river in the world to flow entirely within one country?",
-        answers: {
             a: "Yangtze",
             b: "Mekong",
             c: "Yellow",
-            d: "Ganges"
-        },
-
-        correctAnswer: "a"
+            d: "Ganges",
+        answer: "a",
     },
 
     {
         question: "What Carribean country changed its name from Saint-Domingue following its independence from Napolean Bonaparte and his French forces in 1903?",
-        answers: {
             a: "Haiti",
             b: "Martinique",
             c: "St Martin",
-            d: "Guadeloupe"
-        },
-
-        correctAnswer: "a"
+            d: "Guadeloupe",
+        answer: "a",
     },
 
     {
         question: "At almost 26,000 square kilometers (10,000 sq. miles), what island is the largest in the Mediterranean Sea?",
-        answers: {
             a: "Sardinia",
             b: "Cyprus",
             c: "Crete",
-            d: "Sicily"
-        },
-
-        correctAnswer: "d"
+            d: "Sicily",
+        answer: "d",
     },
 ];
